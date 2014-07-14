dbc516171e7998b0b47a510fdabba413d49cedb0
Mike <mike@sachsmedia.com>
2014-07-14 09:15:09 -0400

    conditional content if post is a contractor single

--- wp-content/themes/evestflorida/single.php
+++ wp-content/themes/evestflorida/single.php
@@ -1,10 +1,25 @@
 
 <?php get_header(); ?>
 
-<div id="left" class="eleven columns" >
+
 	<?php if (have_posts()) : ?>
 	<?php while (have_posts()) : the_post(); ?>
 		
+		<?php 
+			if ( in_category( 'Contractor' )) { ?>
+				<div class="post" id="post-<?php the_ID(); ?>">
+					<div class="contractor-entry">
+						<h1><?php the_title()?></h1>
+						<?php the_post_thumbnail('full')?>
+						<div class="entry">
+							<?php the_content(); ?>
+						</div>
+					</div>
+				</div>
+		
+		<?php } else { ?>
+	
+	<div id="left" class="eleven columns" >
 		<div class="post" id="post-<?php the_ID(); ?>">
 			<div class="title">
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
@@ -12,18 +27,19 @@
 			</div>
 		
 			<div class="entry">
-					
-					<?php the_content('Read the rest of this entry &raquo;'); ?>
-					<div class="clear"></div>
-					<?php wp_link_pages(array('before' => '<p><strong>Pages: </strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+				<?php the_content('Read the rest of this entry &raquo;'); ?>
+				<div class="clear"></div>
+				<?php wp_link_pages(array('before' => '<p><strong>Pages: </strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 			</div>
 		</div>
 		
-	<?php comments_template(); ?>
-	<div class="back"><a href="<?php echo home_url( '/news' ); ?>">&larr; Back to News</a></div>
+		<?php comments_template(); ?>
+		<div class="back"><a href="<?php echo home_url( '/news' ); ?>">&larr; Back to News</a></div>
+	</div>
+		<?php get_sidebar(); ?>
+	
+		<?}?>
+	
 	<?php endwhile; endif; ?>	
-</div>
 
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
